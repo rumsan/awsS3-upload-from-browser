fbe8697cf6306620512f21cc6365c54770f90b9a
Santosh Shrestha <santosh@rumsan.com>
2019-11-20 23:44:46 +0545

    finished

--- app.js
+++ app.js
@@ -1,10 +1,30 @@
 const express = require("express");
-const awsS3 = require("./aws_s3");
+const bodyParser = require("body-parser");
+const AWSS3 = require("./aws_s3");
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => res.send("Hello World!"));
-app.get("/getpolicy", (req, res) => res.send("Hello World!"));
-app.post("/savelocation", (req, res) => res.send("Hello World!"));
+//Update this information with yours;
+AWSCredentials = {
+  bucket: "<<bucket name>>",
+  accessKey: "<<aws access key>>",
+  secret: "<<aws secret>>",
+  region: "<<aws region name>>"
+};
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.get("/", (req, res) => res.sendFile(__dirname + "/index.html"));
+
+app.post("/getpolicy", (req, res) => {
+  let s3 = new AWSS3(AWSCredentials);
+  res.json(s3.getPolicy(req.body));
+});
+
+app.post("/savelocation", (req, res) => {
+  console.log(req.body.location);
+  res.send(`AWS S3 location: <a href="${req.body.location}">${req.body.location}</a>`);
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
